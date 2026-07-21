fa70bc046d78905d38be4f7643a7de054aa32ff5
Koustav Chakraborty <ckoustav7@gmail.com>
2026-07-21 20:19:15 +0530

    Initialize Spring Boot Notification Hub project

---
+++ src/main/java/io/github/ronycce445/notification_hub/NotificationHubApplication.java
@@ -0,0 +1,14 @@
+package io.github.ronycce445.notification_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationHubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NotificationHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ronycce445/notification_hub/NotificationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ronycce445.notification_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
