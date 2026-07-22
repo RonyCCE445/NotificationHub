d16ac1e92a74e501d26ed50db50cc1c7a216a80a
Koustav Chakraborty <ckoustav7@gmail.com>
2026-07-23 01:21:18 +0530

    Implement first REST endpoint with Spring MVC

---
+++ src/main/java/io/github/ronycce445/notification_hub/controller/NotificationController.java
@@ -0,0 +1,22 @@
+package io.github.ronycce445.notification_hub.controller;
+
+import io.github.ronycce445.notification_hub.service.NotificationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/notifications/test")
+    public String testNotification(){
+        return notificationService.sendNotification();
+    }
+
+
+}
