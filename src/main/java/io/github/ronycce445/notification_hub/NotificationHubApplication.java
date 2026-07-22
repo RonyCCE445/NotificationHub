61e37c672cb7dbbc63c39be74058bb2fec866f0a
Koustav Chakraborty <ckoustav7@gmail.com>
2026-07-22 12:38:44 +0530

    Introduce first Spring service bean with constructor injection

--- src/main/java/io/github/ronycce445/notification_hub/NotificationHubApplication.java
+++ src/main/java/io/github/ronycce445/notification_hub/NotificationHubApplication.java
@@ -1,14 +1,33 @@
 package io.github.ronycce445.notification_hub;
 
+import io.github.ronycce445.notification_hub.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class NotificationHubApplication {
+public class NotificationHubApplication implements CommandLineRunner {
+	private final NotificationService notificationService;
+
+	public NotificationHubApplication(NotificationService notificationService) {
+		this.notificationService = notificationService;
+	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(NotificationHubApplication.class, args);
+
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+
+		System.out.println(notificationService.sendNotification());
+
+	}
 }
+
+
---
+++ src/main/java/io/github/ronycce445/notification_hub/service/NotificationService.java
@@ -0,0 +1,9 @@
+package io.github.ronycce445.notification_hub.service;
+
+import org.springframework.stereotype.Service;
+@Service
+public class NotificationService {
+    public String sendNotification(){
+        return "Notification has been sent.";
+    }
+}
